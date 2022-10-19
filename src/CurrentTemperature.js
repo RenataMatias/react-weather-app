81efd0ad0269076a3f4415a999d6cb6a121be8c3
RenataMatias <112482631+RenataMatias@users.noreply.github.com>
2022-10-19 20:21:38 +0100

    bug solved

--- src/CurrentTemperature.js
+++ src/CurrentTemperature.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-export default function (props) {
+export default function CurrentTemperature(props) {
   const [unit, setUnit] = useState("celsius");
 
   function fahrenheit() {
