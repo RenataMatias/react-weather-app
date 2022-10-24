9eff91a3c10b2e903954b1501042a71f68ebf1ad
RenataMatias <112482631+RenataMatias@users.noreply.github.com>
2022-10-24 18:58:39 +0100

    footer updated

--- src/App.js
+++ src/App.js
@@ -8,22 +8,32 @@ export default function App() {
         {/* <h1>Weather App</h1> */}
         <Search defaultCity="Lisbon" />
         <footer>
-          <a
-            href="https://github.com/RenataMatias/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            Open-source code
-          </a>{" "}
-          by{" "}
+          This projec was coded by
           <a
             href="https://www.linkedin.com/in/rematias/"
             target="_blank"
             rel="noreferrer"
           >
             Renata Matias{" "}
+          </a>{" "}
+          and is
+          <a
+            href="https://github.com/RenataMatias/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://dynamic-alfajores-4e8072.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
           </a>
+          .
         </footer>
       </div>
     </div>
